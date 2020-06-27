05b0dc2cde962f29b65026e082b15735412d4a68
ForteScarlet <ForteScarlet@163.com>
2020-06-28 01:06:03 +0800

    update test and doc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # debugger 模组
 为simple-robot提供一些debug功能支持，例如debug启动器、debug封装类、远程debug客户端/服务端等。
+
+具体描述文档可查看：
+https://www.kancloud.cn/forte-scarlet/simple-coolq-doc/1680750
+
+或
+
+http://simple-robot-doc.forte.love/1680750
--- simple-robot-module-debugger-client/src/test/java/test/DebugClientRunApplication.java
+++ simple-robot-module-debugger-client/src/test/java/test/DebugClientRunApplication.java
@@ -9,6 +9,8 @@ import com.simbot.modules.debugger.common.DebugApplication;
 import com.simbot.modules.debugger.common.DebugContext;
 import com.simbot.modules.debugger.common.DebugController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author ForteScarlet <[email]ForteScarlet@163.com>
  * @since JDK1.8
@@ -16,30 +18,31 @@ import com.simbot.modules.debugger.common.DebugController;
 @SimpleRobotApplication
 public class DebugClientRunApplication {
     public static void main(String[] args) throws InterruptedException {
-        DebugContext run = new DebugApplication().run(DebugClientRunApplication.class, args);
+        DebugContext context = new DebugApplication().run(DebugClientRunApplication.class, args);
 
-        DependCenter center = run.getDependCenter();
-        BotManager botManager = run.getBotManager();
-
+        // 看看 debugger 注册了什么bot info
+        BotManager botManager = context.getBotManager();
         for (BotInfo bot : botManager.bots()) {
             System.out.println(bot.getSender().GETTER.getLoginQQInfo());
         }
 
-        System.out.println("sleep...");
-        Thread.sleep(5000);
+        // 获取client中的控制器，功能与server中的控制台一致，用于向server端提交事件。
+        DebugController controller = context.getController();
 
-        System.out.println("get...");
-        DebugController controller = center.get(DebugController.class);
-
-        System.out.println(controller);
-
+        // 如果连接存活，则每2秒发送一次随机的私信消息。
         while(controller.isActive()){
-            controller.send(MsgGetTypes.privateMsg);
-            System.out.println("send");
-            Thread.sleep(2000);
+            controller.send(MsgGetTypes.privateMsg, msg -> {
+                // 将msg中的thisCode设置为统一的账号：114514
+                // 实际情况下应该设置为与你的远程端一致的账号信息
+                msg.setDgThisCode("114514");
+                // msg.setThisCode("114514"); // 上下两个方法一样效果
+
+            });
+            System.out.println("send private msg");
+            TimeUnit.SECONDS.toMillis(2);
         }
 
+        // 连接断开则会跳出循环
         System.out.println("out - not active");
-
     }
 }
